9f8424747479c9d5f84ef8c7e6073104acd3698a
marielle-lopez <mariellelouisse@gmail.com>
2025-02-19 19:30:02 +1100

    Add conditions for error appearances, add dummy tags, use Zod for `Post` schema validation

--- src/pages/CreatePostPage/CreatePostPage.tsx
+++ src/pages/CreatePostPage/CreatePostPage.tsx
@@ -1,30 +1,50 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { postSchema } from '../../schemas/post-schema';
+
 const CreatePostPage = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: zodResolver(postSchema),
+    });
+
     return <>
         <h1>Create Post</h1>
-        <form>
+        <form onSubmit={handleSubmit((data) => console.log('Submitted', data))}>
             <div>
                 <label htmlFor="title">Title</label>
-                <input type="text" id="title" name="title" />
+                <input type="text" id="title" {...register('title')} />
+                {errors?.title && <p>{errors.title.message}</p>}
             </div>
             <div>
                 <label htmlFor="author">Author</label>
-                <input type="text" id="author" name="author" />
+                <input type="text" id="author" {...register('author')}/>
+                {errors?.author && <p>{errors.author.message}</p>}
             </div>
             <div>
                 <label htmlFor="introduction">Introduction</label>
-                <textarea id="introduction" name="introduction"></textarea>
+                <textarea id="introduction" {...register('introduction')}></textarea>
+                {errors?.introduction && <p>{errors.introduction.message}</p>}
             </div>
             <div>
                 <label htmlFor="body">Body</label>
-                <textarea id="body" name="body"></textarea>
+                <textarea id="body" {...register('body')}></textarea>
+                {errors?.body && <p>{errors.body.message}</p>}
             </div>
             <div>
                 <label htmlFor="conclusion">Conclusion</label>
-                <textarea id="conclusion" name="conclusion"></textarea>
+                <textarea id="conclusion" {...register('conclusion')}></textarea>
+                {errors?.conclusion && <p>{errors.conclusion.message}</p>}
             </div>
             <div>
                 <label htmlFor="tags">Tags</label>
-                <input type="text" id="tags" name="tags" />
+                <select id="tags" {...register('tags')} multiple>
+                    <option value="cooking">Cooking</option>
+                    <option value="secret">Secret</option>
+                    <option value="spongebob">Spongebob</option>
+                </select>
+                {errors?.tags && <p>{errors.tags.message
+                    ? errors.tags.message
+                    : 'At least one tag is required'}</p>}
             </div>
             <button type="submit">Create Post</button>
         </form>

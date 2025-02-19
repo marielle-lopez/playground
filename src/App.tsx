946ec3ecbe975a30a914164c94247addb0983d58
marielle-lopez <mariellelouisse@gmail.com>
2025-02-20 01:15:32 +1100

    Use `PostsContextProvider`

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage.tsx';
 import CreatePostPage from './pages/CreatePostPage/CreatePostPage.tsx';
 import NavigationBar from './components/NavigationBar/NavigationBar.tsx';
+import PostsContextProvider from './contexts/PostsContextProvider.tsx';
 
 function App() {
   return (
-    <BrowserRouter>
-    <NavigationBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/createPost" element={<CreatePostPage />} />
-      </Routes>
-    </BrowserRouter>
+    <PostsContextProvider>
+      <BrowserRouter>
+      <NavigationBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/createPost" element={<CreatePostPage />} />
+        </Routes>
+      </BrowserRouter>
+    </PostsContextProvider>
   )
 }
 

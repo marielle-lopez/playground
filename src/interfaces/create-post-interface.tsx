95fdf37c333c8012ea8c95b99ecf5d9cbd167765
marielle-lopez <mariellelouisse@gmail.com>
2025-02-19 20:43:31 +1100

    Add interface for created posts

---
+++ src/interfaces/create-post-interface.tsx
@@ -0,0 +1,10 @@
+interface CreatePost {
+    title: string;
+    author: string;
+    introduction: string;
+    body: string;
+    conclusion: string;
+    tags: string[];
+};
+
+export default CreatePost;

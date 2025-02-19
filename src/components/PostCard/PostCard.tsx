c0a2d6eb316f4f86a20c59eaaf168df66eb051f2
marielle-lopez <mariellelouisse@gmail.com>
2025-02-20 01:16:44 +1100

    Use `PostsContext` to refresh `posts` state on post delete

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,11 +1,15 @@
+import { useContext } from "react";
 import Post from "../../interfaces/post-interface";
-import { deletePost } from "../../services/post-service";
+import { deletePost, getAllPosts } from "../../services/post-service";
+import { PostsContext } from "../../contexts/PostsContextProvider";
 
 interface PostCardProps {
     post: Post;
 }
 
 const PostCard = ({ post }: PostCardProps) => {
+    const { setPosts } = useContext(PostsContext);
+
     return <div>
         <h1>{post.title}</h1>
         <p>{post.createdAt.toDateString()}</p>
@@ -17,7 +21,13 @@ const PostCard = ({ post }: PostCardProps) => {
         <ul>
             {post.tags.map((tag) => <li key={tag}>{tag}</li>)}
         </ul>
-        <button onClick={() => deletePost(post.id)}>Delete</button>
+        <button onClick={
+            () => deletePost(post.id)
+                .then(() => getAllPosts()
+                    .then((res) => setPosts(res))
+                    .catch((err) => console.error(err.message)))}>
+            Delete
+        </button>
     </div>
 };
 

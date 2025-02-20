5d4d8d4818d530cd9625b3acc010d0295666e1b9
marielle-lopez <mariellelouisse@gmail.com>
2025-02-20 18:57:45 +1100

    Add loading display when post is being retrieved, add button to delete post, add button to change to 'Edit' mode

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,21 +1,35 @@
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import Post from '../../interfaces/post-interface';
-import { getPost } from '../../services/post-service';
+import { deletePost, getAllPosts, getPost } from '../../services/post-service';
+import { PostsContext } from '../../contexts/PostsContextProvider';
 
 const PostPage = () => {
     const { id } = useParams();
+    const { setPosts } = useContext(PostsContext);
     const [post, setPost] = useState<Post | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [editMode, setEditMode] = useState<boolean>(false);
 
     useEffect(() => {
         getPost(Number(id))
             .then((res) => setPost(res))
-            .catch((err) => console.error(err));
-    }, [])
+            .catch((err) => console.error(err))
+            .finally(() => setLoading(false));
+    }, [loading])
 
     return <>
-        {post 
-            ? <>
+        {!post && !loading &&
+            <>
+                <h1>Post not found</h1>
+                <p>Sorry, we couldn't find the post you were looking for.</p>
+            </>
+        }
+        {!post && loading &&
+            <p>Loading...</p>
+        }
+        {post && !editMode &&
+            <>
                 <h1>{post.title}</h1>
                 <p>{post.createdAt.toDateString()}</p>
                 <p>{post.modifiedAt.toDateString()}</p>
@@ -26,8 +40,19 @@ const PostPage = () => {
                 <ul>
                     {post.tags.map((tag) => <li key={tag}>{tag}</li>)}
                 </ul>
-              </>
-            : <h1></h1>}
+                <button onClick={
+                    () => deletePost(post.id)
+                        .then(() => getAllPosts()
+                            .then((res) => setPosts(res))
+                            .catch((err) => console.error(err.message)))}>
+                    Delete
+                </button>
+                <button onClick={() => setEditMode(true)}>Edit</button>
+            </>
+        }
+        {post && editMode &&
+            <p>Edit mode</p>
+        }
     </>
 };
 

f892ed23bfefec2ea2233c5db82bf1ab19f7a63e
marielle-lopez <mariellelouisse@gmail.com>
2025-02-20 18:55:20 +1100

    Update post information to display in `PostCard` component

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -13,12 +13,9 @@ const PostCard = ({ post }: PostCardProps) => {
 
     return <div>
         <h1>{post.title}</h1>
-        <p>{post.createdAt.toDateString()}</p>
         <p>{post.modifiedAt.toDateString()}</p>
         <p>{post.author}</p>
         <p>{post.introduction}</p>
-        <p>{post.body}</p>
-        <p>{post.conclusion}</p>
         <ul>
             {post.tags.map((tag) => <li key={tag}>{tag}</li>)}
         </ul>
